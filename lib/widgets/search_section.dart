4925635bbb7b7d2c75a59902e7393b0108132081
notYouralex <alexanderlarche@gmail.com>
2025-11-18 16:06:49 +0800

    created app bar

--- lib/widgets/search_section.dart
+++ lib/widgets/search_section.dart
@@ -7,39 +7,7 @@ class SearchSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 40, left: 16, right: 20),
-      child: TextField(
-        decoration: InputDecoration(
-          prefixIcon: Container(
-            margin: EdgeInsets.all(10),
-            child: SvgPicture.asset('assets/icons/Search.svg',),
-          ), 
-          suffixIcon: Container(
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.only(right: 8),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SvgPicture.asset('assets/icons/Line_18.svg',),
-                SizedBox(width: 10,),
-                SvgPicture.asset('assets/icons/Filter.svg',)
-              ],
-            ),
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
-            borderSide: BorderSide.none,
-          ),
-          filled: true,
-          fillColor: Colors.white,
-          hint: Text('Search Pancake...',
-            style: TextStyle(
-              fontSize: 14,
-              color: Color(0xFFBDBDBD),
-            ),
-            )
-        ),
-      ),
+      
     );
   }
 }
