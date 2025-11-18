4d7b6debbf6f532c8d407c3f864716dc8fcd8ec1
notYouralex <alexanderlarche@gmail.com>
2025-11-18 17:03:17 +0800

    created search section

--- lib/widgets/search_section.dart
+++ lib/widgets/search_section.dart
@@ -7,7 +7,29 @@ class SearchSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      margin: EdgeInsets.only(left: 20, right: 20, top: 40),
+      child: TextField(
+        decoration: InputDecoration(
+          prefixIcon: Container(
+            margin: EdgeInsets.all(10),
+            child: SvgPicture.asset("assets/icons/Search.svg")
+            ),
+          suffixIcon: Container(
+            margin: EdgeInsets.all(10),
+            child: SvgPicture.asset("assets/icons/Filter.svg")
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(14),
+            borderSide: BorderSide.none
+          ),
+          hint: Text("Search",
+          style: TextStyle(
+            color: Color(0xFFDDDADA)
+          ),),
+          fillColor: Colors.white,
+          filled: true
+        ),
+      ),
     );
   }
 }
